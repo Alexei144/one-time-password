cda109ca5d5edd76ec6c61bd9551bde2d833a407
Alexei Fazlyev <alexeifer@yandex.ru>
2024-09-02 09:12:58 +0300

    initial commit

---
+++ src/main/java/ru/fazlyev/onetimepassword/OneTimePasswordApp.java
@@ -0,0 +1,12 @@
+package ru.fazlyev.onetimepassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTimePasswordApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneTimePasswordApp.class);
+    }
+}
